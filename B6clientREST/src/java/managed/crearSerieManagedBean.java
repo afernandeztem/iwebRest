b931ae28755d3ee3d6377bb6651ebb896837800e
francis1196 <francis8rs@gmail.com>
2018-12-09 13:25:50 +0100

    arreglo en mis series

--- B6clientREST/src/java/managed/crearSerieManagedBean.java
+++ B6clientREST/src/java/managed/crearSerieManagedBean.java
@@ -112,6 +112,6 @@ public class crearSerieManagedBean {
         serieCliente = new SerieClient();
         serieCliente.create_XML(serie);
 
-        return "index?faces-redirect=true";
+        return "misSeries?refresh=1&faces-redirect=true";
     }
 }
